a22d24a52ad7966e04ad43d0336faf56371723b5
codyRhett <artemryvkin@yandex.ru>
2021-11-02 20:00:51 +0300

    items 1,2,3 of HW02 were done

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,9 +5,11 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -40,7 +42,7 @@ public class InMemoryMealRepository implements MealRepository {
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+        return repository.values().stream().sorted(Comparator.comparing(Meal::getDateTime).reversed()).collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -1,4 +1,22 @@
 package ru.javawebinar.topjava.util;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
 public class UsersUtil {
+    public static final List<User> users = Arrays.asList(
+            new User(1, "Sergey", "sergey@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(2, "Artem", "Artem@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(3, "Tanya", "Tanya@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(4, "Sasha", "Sasha@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(5, "Lena", "Lena@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(6, "Katya", "Katya@yandex.ru", "12345", Role.USER, Role.values()),
+            new User(7, "John", "John@yandex.ru", "12345", Role.USER, Role.values())
+    );
 }
